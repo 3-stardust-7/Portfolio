99fa3d26c5925f34657a1888bca82366f3864093
Kirubha V <vkirubha7@gmail.com>
2025-03-24 19:06:39 +0530

    installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Your React or other component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
